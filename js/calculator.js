dbabf30e00fa6f8e5cdff236bbd31fa49c38f8cf
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-20 08:59:55 -0600

    working on stylesheet for calculator

---
+++ js/calculator.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+}
+
+function updateDisplay() {
+    const display =;
+}
